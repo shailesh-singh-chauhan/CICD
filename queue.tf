26da4a34ab2a2b8474b1fa49385d377ff9424a8a
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2023-08-03 17:59:33 +0530

    Update queue.tf

--- queue.tf
+++ queue.tf
@@ -1,5 +1,5 @@
-resource "genesyscloud_routing_queue" "example_queue" {
-  name                              = "Example Queue"
+resource "genesyscloud_routing_queue" "example_queue_2" {
+  name                              = "Example Queue 2"
   division_id                       = "54d8ea56-684f-4c16-b2cf-87cdd614e601"
   description                       = "This is an example description"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
