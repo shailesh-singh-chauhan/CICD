dc68d16579a31f7752a3d8af79eeb7fad65a7a5f
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2023-08-04 14:19:56 +0530

    Update main_var.tf

--- main_var.tf
+++ main_var.tf
@@ -1,7 +1,3 @@
-# Module for routing-queue
-module "routing_queue" {
-    source = "./modules/routing_queue"
-	
 resource "genesyscloud_flow" "flowname" {
   filepath = "./ShaileshTest.yaml"
   #file_content_hash = filesha256("./filename.yaml") 
