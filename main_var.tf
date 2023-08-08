f8512bb0890616aaeed2d7d9ebd2ca0ff5d2fbaf
Singh <shailesh.h.singh@accenture.com>
2023-08-08 15:07:43 +0530

    organised structure

--- main_var.tf
+++ main_var.tf
@@ -1,4 +1,15 @@
+# Module for auth-division
+module "auth_division" {
+  source = "./modules/auth_division"   
+}
+
+
+# Module for routing-queue
+module "routing_queue" {
+    source = "./modules/routing_queue"
+
+#Module for workflow
 resource "genesyscloud_flow" "flowname" {
-  filepath = "./ShaileshTest.yaml"
+  filepath = "./modules/workflow/ShaileshTest.yaml"
   file_content_hash = filesha256("./ShaileshTest.yaml") 
 }
