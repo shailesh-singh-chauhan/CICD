90a4b5ed4b4cff202cecb2bc92e6f8997a233f37
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2023-08-08 15:20:17 +0530

    Update main_var.tf

--- main_var.tf
+++ main_var.tf
@@ -7,7 +7,7 @@ module "auth_division" {
 # Module for routing-queue
 module "routing_queue" {
     source = "./modules/routing_queue"
-
+}
 #Module for workflow
 resource "genesyscloud_flow" "flowname" {
   filepath = "./modules/workflow/ShaileshTest.yaml"
