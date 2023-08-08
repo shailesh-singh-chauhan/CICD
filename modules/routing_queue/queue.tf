6c2c4c84c644437811887c41f009c6fa938ea3a3
shailesh-singh-chauhan <59451782+shailesh-singh-chauhan@users.noreply.github.com>
2023-08-08 15:35:46 +0530

    Update queue.tf

--- modules/routing_queue/queue.tf
+++ modules/routing_queue/queue.tf
@@ -1,6 +1,6 @@
 resource "genesyscloud_routing_queue" "example_queue_2" {
   name                              = "Example Queue 2"
-  division_id                       =  genesyscloud_auth_division.default-home.id
+  division_id                       =  genesyscloud_auth_division.home.id
   description                       = "This is an example description"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
   acw_timeout_ms                    = 300000
